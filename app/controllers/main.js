35a13b54f629591d54de1cc35bb6c2ed70d91717
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2015-01-27 02:18:05 +0200

    make query easier
    sorted by code number by default
    you can use 'skip' and 'limit' parameters

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -14,15 +14,27 @@ exports.schedule = function (req, res, next) {
       return next(new Error('Server Error'));
     }
     var $ = cheerio.load(body);
-    var courses = $('table tr').slice(1).map(function (i, e) {
-      var tds = $(e).find('td');
-      return {
-        type: $(tds[0]).text(),
-        code: $(tds[1]).text(),
-        title: $(tds[2]).text(),
-        people: $(tds[3]).text()
-      };
-    }).get();
+    var courses = $('table tr')
+      .slice(1) //removed first item {title bar of table}
+      .map(function (i, e) {
+        var tds = $(e).find('td');
+        return {
+          type: $(tds[0]).text(),
+          code: Number($(tds[1]).text()),
+          title: $(tds[2]).text(),
+          people: $(tds[3]).text()
+        };
+      })
+      .get() // get basic JSONArray
+      .sort(function (a, b) { // sort by code
+        return a.code - b.code;
+      });
+    if (req.query.skip) {
+      courses = courses.slice(req.query.skip);
+    }
+    if (req.query.limit) {
+      courses = courses.slice(0, req.query.limit);
+    }
     return res.json(courses);
   });
 };
